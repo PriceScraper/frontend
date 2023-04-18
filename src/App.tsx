05c25d516ac6ef852059fa474d902dab0b423e91
Sander Constantin <sander.constantin@student.kdg.be>
2023-04-18 13:43:53 +0200

    feat: test to pre-receive
    This commit should be sent

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import {Box, Container, CssBaseline} from "@mui/material";
 import Toolbar from "@mui/material/Toolbar";
 
 
+
 function App() {
     return <BrowserRouter>
         <Box sx={{display: 'flex'}}>
