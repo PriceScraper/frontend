35e9aa40ac1ad2d54e0c21bbad5c4393e87b0a6f
Sander Constantin <sander.constantin@student.kdg.be>
2023-04-18 13:41:35 +0200

    feat: test to pre-receive
    This commit should be sent

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Navigation from './components/navigation/Navigation';
 import {Box, Container, CssBaseline} from "@mui/material";
 import Toolbar from "@mui/material/Toolbar";
 
+
 function App() {
     return <BrowserRouter>
         <Box sx={{display: 'flex'}}>
