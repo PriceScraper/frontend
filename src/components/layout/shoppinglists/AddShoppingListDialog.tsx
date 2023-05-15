401c4451f64021b8cdf3188da253225ba21339c4
Sander Constantin <sander.constantin@student.kdg.be>
2023-05-15 12:47:32 +0200

    fix: warning

--- src/components/layout/shoppinglists/AddShoppingListDialog.tsx
+++ src/components/layout/shoppinglists/AddShoppingListDialog.tsx
@@ -1,15 +1,9 @@
-import {
-  Button,
-  Dialog,
-  DialogContent,
-  DialogTitle,
-  TextField,
-} from "@mui/material";
+import {Button, Dialog, DialogContent, DialogTitle, TextField,} from "@mui/material";
 import React from "react";
-import { SlideTransition as Transition } from "../transitions/Transitions";
-import { useForm } from "react-hook-form";
-import { createShoppingListForUser } from "../../../services/shoppinglist.service";
-import { useMutation, useQueryClient } from "react-query";
+import {SlideTransition as Transition} from "../transitions/Transitions";
+import {useForm} from "react-hook-form";
+import {createShoppingListForUser} from "../../../services/shoppinglist.service";
+import {useMutation, useQueryClient} from "react-query";
 
 interface AddShoppingListDialogProps {
   handleCloseDialog: () => void;
@@ -24,10 +18,10 @@ export default function AddShoppingListDialog({
   const mutation = useMutation({
     mutationFn: (title: string) => createShoppingListForUser(title),
     onSuccess: async () => {
-      await queryClient.invalidateQueries({ queryKey: ["shoppingLists"] });
+      await queryClient.invalidateQueries({queryKey: ["shoppingLists"]});
     },
   });
-  const { register, handleSubmit, control } = useForm();
+  const {register, handleSubmit} = useForm();
   return (
     <Dialog
       open={dialogIsOpen}
