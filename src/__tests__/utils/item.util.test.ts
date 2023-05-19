f967a940e2418b8244f953e26e2f0590e2f1d1ee
Sander Constantin <sander.constantin@student.kdg.be>
2023-05-19 10:31:48 +0200

    fix: Display correct price

--- src/__tests__/utils/item.util.test.ts
+++ src/__tests__/utils/item.util.test.ts
@@ -1,9 +1,9 @@
-import { findTrackedItemWithLowestPrice } from "../../utils/item.util";
-import { TrackedItem } from "../../models/TrackedItem";
-import { Shop } from "../../models/Shop";
-import { ItemPrice } from "../../models/ItemPrice";
-import { Item } from "../../models/Item";
-import { ItemUnit } from "../../models/ItemUnit";
+import {findTrackedItemWithLowestPrice, onePricePerDay} from "../../utils/item.util";
+import {TrackedItem} from "../../models/TrackedItem";
+import {Shop} from "../../models/Shop";
+import {ItemPrice} from "../../models/ItemPrice";
+import {Item} from "../../models/Item";
+import {ItemUnit} from "../../models/ItemUnit";
 
 test("findTrackedItemWithLowestPrice returns lowest price", () => {
   const trackedItems = [
@@ -35,9 +35,20 @@ test("findTrackedItemWithLowestPrice with empty tracked items should throw", () 
 });
 
 test("get whitelisted tracked items should return tracked item", () => {
-  const item = new Item(1, "", "", "", 1, new ItemUnit(1, 1), "", [
+  const i = new Item(1, "", "", "", 1, new ItemUnit(1, 1), "", [
     new TrackedItem("", new Shop(1, "aldi.be", ""), null, [
       new ItemPrice(1, null, "", 1.75),
     ]),
   ]);
+  expect(i.id).toBe(1)
 });
+
+test("one price per day", () => {
+  const prices: ItemPrice[] = [
+    {id: 1, price: 3, timestamp: "2023-04-05T01:01:01", item: null},
+    {id: 2, price: 3, timestamp: "2023-04-05T02:01:01", item: null},
+    {id: 3, price: 3, timestamp: "2023-04-06T01:01:01", item: null},
+  ]
+  expect(prices.length).toBe(3)
+  expect(onePricePerDay(prices).length).toBe(2)
+})
--- src/components/item/HistoryChart.tsx
+++ src/components/item/HistoryChart.tsx
@@ -1,8 +1,8 @@
 import {TrackedItem} from "../../models/TrackedItem";
-import {ItemPrice} from "../../models/ItemPrice";
 import {VictoryChart, VictoryLegend, VictoryLine, VictoryTheme} from "victory";
 import {getShopDominantColorByName} from "../../utils/shop.util";
 import {Typography} from "@mui/material";
+import {onePricePerDay, orderedPrices} from "../../utils/item.util";
 
 export default function HistoryChart(props: { trackedItems: TrackedItem[] }) {
     if (props.trackedItems.filter(ti => ti.itemPrices.length > 1).length === 0) return <></>
@@ -19,14 +19,14 @@ export default function HistoryChart(props: { trackedItems: TrackedItem[] }) {
                            orientation="horizontal"
                            gutter={20}
                            style={{border: {stroke: "black"}, title: {fontSize: 20}}}
-                           data={props.trackedItems.map(ti => {
+                           data={props.trackedItems.filter(e => e.itemPrices.length > 1).map(ti => {
                                return {
                                    name: ti.shop.name,
                                    symbol: {fill: getShopDominantColorByName(ti.shop.name)}
                                }
                            })}
             />
-            {props.trackedItems.map(trackedItem =>
+            {props.trackedItems.filter(e => e.itemPrices.length > 1).map(trackedItem =>
                 <VictoryLine
                     key={trackedItem.url}
                     style={{
@@ -38,7 +38,7 @@ export default function HistoryChart(props: { trackedItems: TrackedItem[] }) {
                             strokeLinecap: "round"
                         },
                     }}
-                    data={orderedPrices(trackedItem.itemPrices).map(e => {
+                    data={orderedPrices(onePricePerDay(trackedItem.itemPrices)).map(e => {
                         return {x: e.timestamp.substring(0, 10), y: e.price}
                     })}
                     interpolation={"basis"}
@@ -46,13 +46,4 @@ export default function HistoryChart(props: { trackedItems: TrackedItem[] }) {
             }
         </VictoryChart>
     </>
-}
-
-function orderedPrices(prices: ItemPrice[]) {
-    return prices
-        .sort((a, b) => {
-            if (new Date(a.timestamp).getTime() > new Date(b.timestamp).getTime()) return 1
-            if (new Date(a.timestamp).getTime() < new Date(b.timestamp).getTime()) return -1
-            return 0
-        })
 }
