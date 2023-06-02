8095a2d766fcf3466cc46a2eafe654cc9d2285e4
Sander Constantin <sander.constantin@student.kdg.be>
2023-06-02 14:25:13 +0200

    fix: typo

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -4,9 +4,10 @@ import {getAccessToken} from "../functions/authTokenStorage";
 axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL
 export default function initAxiosInterceptors() {
     axios.interceptors.request.clear()
+    axios.defaults.timeout = 120000
     axios.interceptors.request.use((conf) => {
         const token = getAccessToken()
-        if(token !== null) conf.headers.Authorization = `${token}`
+        if (token !== null) conf.headers.Authorization = `${token}`
         return conf
     })
 }
