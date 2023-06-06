1c07951068711d1d6986b1675db972484ae1d38d
Sander Constantin <sander.constantin@student.kdg.be>
2023-06-06 16:07:13 +0200

    fix: Layout zoekpagina

--- src/hooks/usePreparedMediaQuery.ts
+++ src/hooks/usePreparedMediaQuery.ts
@@ -1,9 +1,11 @@
 import {useMediaQuery} from "@mui/material";
 
+const extraBigScreenQuery = "(min-width: 1200px)"
 const bigScreenQuery = "(min-width: 1000px)"
 const smallScreenQuery = "(max-width: 500px)"
 export default function usePreparedMediaQuery() {
     const isBigScreen = useMediaQuery(bigScreenQuery)
+    const isExtraBigScreen = useMediaQuery(extraBigScreenQuery)
     const isSmallScreen = useMediaQuery(smallScreenQuery)
-    return {isBigScreen, isSmallScreen, isMediumScreen: (!isBigScreen && !isSmallScreen)}
+    return {isBigScreen, isSmallScreen, isMediumScreen: (!isBigScreen && !isSmallScreen), isExtraBigScreen}
 }
