4a8c0f5d7073d1617baa5644d025b4eb6c03b8ee
TimTrademark <truyens.tim@telenet.be>
2023-06-06 11:07:48 +0200

    feat: scan product design/layout finished

--- src/pages/items/ScanItem.tsx
+++ src/pages/items/ScanItem.tsx
@@ -1,49 +1,79 @@
-import {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import MainContainer from "../../components/layout/MainContainer";
 import axios from "axios";
-import {Button, Typography} from "@mui/material";
+import { Button, Tooltip, Typography } from "@mui/material";
 import ScanBarcodeFromCamera from "../../components/item/ScanBarcodeFromCamera";
-import {useSnackbar} from "notistack";
+import { useSnackbar } from "notistack";
 
 export default function ScanItem() {
-    const navigate = useNavigate()
-    const [error, setError] = useState(false)
-    const {enqueueSnackbar} = useSnackbar()
+  const navigate = useNavigate();
+  const [error, setError] = useState(false);
+  const { enqueueSnackbar } = useSnackbar();
+  const [scanTooltipOpen, setScanTooltipOpen] = useState<boolean>(false);
 
-    function onSuccess(value: string) {
-        if (value.length === 13) {
-            setError(false)
-            axios
-                .get<{ succeeded: boolean, result: string }>(`/item/scan?q=${value}`)
-                .then(res => {
-                    if (res.data.succeeded) navigate(`/item/${res.data.result}`)
-                    else setError(true)
-                })
-                .catch(err => onError(`${err.message}`))
-        } else {
-            onError(`Barcode has wrong format. Barcode: ${value}`)
-        }
+  useEffect(() => {
+    setTimeout(() => {
+      setScanTooltipOpen(true);
+      console.log("tooltip open");
+    }, 7000);
+  }, []);
+
+  function onSuccess(value: string) {
+    if (value.length === 13) {
+      setError(false);
+      axios
+        .get<{ succeeded: boolean; result: string }>(`/items/code?q=${value}`)
+        .then((res) => {
+          if (res.data.succeeded)
+            navigate(`/items/search?q=${res.data.result}`);
+          else setError(true);
+        })
+        .catch((err) => onError(`${err.message}`));
+    } else {
+      onError(`Barcode has wrong format. Barcode: ${value}`);
     }
+  }
 
-    function onError(err: string) {
-        enqueueSnackbar(`${err}`, {variant: "error"})
-    }
+  function onError(err: string) {
+    enqueueSnackbar(`${err}`, { variant: "error" });
+  }
 
-    return <MainContainer backTrackableTo={"/"} headerMsg={"Scan product"}>
-        <>
-            {error
-                ? <>
-                    <Typography variant={"h6"}>Helaas! We konden geen resultaten vinden voor de gescande
-                        barcode.</Typography>
-                    <br/>
-                    <Button color={"warning"}>Probeer opnieuw</Button>
-                </>
-                : <>
-                    <Typography variant={"subtitle1"}>Houdt de barcode heel dicht bij de camera.</Typography>
-                    <ScanBarcodeFromCamera onSuccess={onSuccess} onError={onError}/>
-                </>
-            }
-        </>
+  return (
+    <MainContainer backTrackableTo={"/"} headerMsg={"Scan product"}>
+      <>
+        {error ? (
+          <>
+            <Typography variant={"h6"}>
+              Helaas! We konden geen resultaten vinden voor de gescande barcode.
+            </Typography>
+            <br />
+            <Button color={"warning"}>Probeer opnieuw</Button>
+          </>
+        ) : (
+          <>
+            <Typography
+              sx={{
+                marginTop: "-0.5rem",
+                marginBottom: "1rem",
+                color: "#343a40",
+              }}
+              variant={"subtitle1"}
+            >
+              Scan de barcode van een product om het op te zoeken
+            </Typography>
+            <Tooltip
+              placement={"top-end"}
+              open={scanTooltipOpen}
+              title={"Breng de barcode dichtbij de camera"}
+            >
+              <div></div>
+            </Tooltip>
+
+            <ScanBarcodeFromCamera onSuccess={onSuccess} onError={onError} />
+          </>
+        )}
+      </>
     </MainContainer>
+  );
 }
